b4740ff93d498bf784b6c82301067c48cfcc3095
Locochoco <ivanrwpf@gmail.com>
2023-07-15 00:05:17 -0300

    changed nixpkg repo reference to master

--- flake.nix
+++ flake.nix
@@ -3,8 +3,7 @@
 
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
-
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/master";
   };
 
   outputs = { self, flake-utils, nixpkgs }:
